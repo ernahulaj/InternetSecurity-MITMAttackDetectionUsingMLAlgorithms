013bc765f2c4ea5cf489e0e72fd87b7bff572f7c
Erza Lumi <erza.lumi@student.uni-pr.edu>
2023-01-08 15:50:36 +0100

    Dataframe creation and concatenation

--- MLDetection.py
+++ MLDetection.py
@@ -30,3 +30,24 @@ read_data.info()
 #Rishikimi i DataFrame-it
 read_data.head()
 
+#Krijimi nje DataFrame te re qe permban vetem kolonen 'authentication' te read_data DataFrame
+data = pd.DataFrame(read_data['authentication'])
+
+#Definimi i funksionit per hashimin e kolonave 
+def hash_col(df, col, N):
+    cols = [col + "_" + str(i) for i in range(N)]
+
+    def xform(x):
+        tmp = [0 for i in range(N)]
+        tmp[hash(x) % N] = 1
+        return pd.Series(tmp,index=cols)
+         
+    df[cols] = df[col].apply(xform)
+    return df.drop(col,axis=1)
+
+#DataFrame qe rezulton sipas funksionit hash_col printohet ne ekran dhe me pas lidhen DataFrames 'data1' me 'read_data'
+print(hash_col(data, 'authentication',8))
+read_data = pd.concat([read_data,data],axis=1)
+print(read_data)
+print(read_data.info())
+
